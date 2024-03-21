1a2a0e820472f9196d154d728d729bd35c3b9285
Imax2001 <iot5037@psu.edu>
2024-03-21 13:04:36 -0400

    Add project files.

---
+++ AERSP_424_Proj.cpp
@@ -0,0 +1,12 @@
+﻿// AERSP_424_Proj.cpp : Defines the entry point for the application.
+//
+
+#include "AERSP_424_Proj.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
---
+++ AERSP_424_Proj.h
@@ -0,0 +1,8 @@
+﻿// AERSP_424_Proj.h : Include file for standard system include files,
+// or project specific include files.
+
+#pragma once
+
+#include <iostream>
+
+// TODO: Reference additional headers your program requires here.
